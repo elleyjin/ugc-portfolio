e7200792f4504f78312a652f6e3a5cecf28f3b54
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-10-14 14:31:06 +0800

    added navbar & header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
+// import "./App.css";
+import UgcPortfolio from "./UgcPortfolio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <UgcPortfolio />
+      </header>
     </div>
   );
 }
--- src/UgcPortfolio.js
+++ src/UgcPortfolio.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Navbar from "./Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import CoverImage from "./image/cover-image.JPG";
+import "./UgcPortfolio.css";
 
 export default function UgcPortfolio() {
-  return <div>"hello from portfolio"</div>;
+  return (
+    <div className="UgcPortfolio">
+      <div className="container-fluid">
+        <Navbar />
+        <div className="row">
+          <div className="col">
+            <h2>Keyboard / Mechanical switch Enthusiast</h2>
+            <img src={CoverImage} alt="keyboard" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
